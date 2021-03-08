50d0779b9a77150ac49d82caf95d77dfc7de4452
Pablo Zadro <pablozadro@gmail.com>
2021-03-08 17:59:04 -0300

    main: wip

--- src/store/store.ts
+++
@@ -1,8 +0,0 @@
-import { createStore } from 'redux';
-import reducer from './reducer';
-
-const store: any = createStore(reducer);
-
-console.log('- store -', store.getState())
-
-export default store;
